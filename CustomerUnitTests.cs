5790356b649f010e61216fcc661013fdc15fbb88
SUYATITECH\sithomas <sithomas@suyati.com>
2019-07-25 21:37:46 +0530

    HLIMS_Unit_Tests
    Unit Tests Created for the API

---
+++ CustomerUnitTests.cs
@@ -0,0 +1,22 @@
+﻿using Xunit;
+using Moq;
+using HLIMS_API_MicroServices.Domain.Services;
+using System.Threading.Tasks;
+namespace HLIMS_API_MICROSERVICES_UNITTESTS
+{
+    public class CustomerUnitTests
+    {
+        
+        [Theory]
+        [InlineData("SBI","SBI_sijuthomasp+1@gmail.com_8848542724_01012019", true)]
+        public void Testing(string bankName, string loanID, bool valueToCheck)
+        {
+           ICustomerService _customerService;
+            var result = _customerService.GetCustomerAsync(bankName, loanID);
+            Assert.True (result.Response.RequestSuccessful);
+
+          
+        }
+       
+    }
+}
